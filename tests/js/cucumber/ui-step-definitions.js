cc777a1f717f5e862b61f5f0df33cc86e55613dd
Yulia Bobkova <y.bobkova@ps-webforge.com>
2014-07-14 18:47:30 +0200

    added steps for text input, buttonstroke, css contents (label, headline)

--- tests/js/cucumber/ui-step-definitions.js
+++ tests/js/cucumber/ui-step-definitions.js
@@ -1,8 +1,29 @@
 module.exports = function(expect) {
-  this.When(/^the user fills "([^"]*)" in the "([^"]*)"\-field$/, function(value, field, callback, withCSS) {
-    var $field = this.css('input[name="'+ field + '"]').exists().get();
+  var fillIn = function(world, field, value, callback) {
+    var $field = world.css('input[name="'+ field + '"]').exists().get();
+    world.util.fill($field, value, callback);
+  }
 
-    this.util.fill($field, value, callback);
-    //this.browser.fill($field.get(0), value);
+  this.When(/^the user fills "([^"]*)" in the "([^"]*)"\-field$/, function(value, field, callback, withCSS) {
+  	fillIn(this, field, value, callback);
+  });
+
+  this.When(/^the user presses the "([^"]*)"\-Button$/, function(button_name, callback, withCSS) {
+    var $button = this.util.textButton(button_name);
+    this.util.pressButton(button_name, callback);
+  });
+
+  this.Then(/^the user sees the headline "([^"]*)"$/, function(headline, callback, withCSS) {
+	this.css('h1:contains("' + headline + '")').exists();
+	callback();
+  });
+
+  this.When(/^the user fills in a wrong username in the "([^"]*)"\-field$/, function(field, callback) {
+  	fillIn(this, field, "areallywrongvalue", callback);
+  });
+
+  this.Then(/^the user must see the label "([^"]*)"$/, function(label, callback, withCSS) {
+  	this.css('body:contains("' + label + '")').exists();
+    callback();
   });
 }

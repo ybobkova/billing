8d71b75641b72db8516ee74657ce7f579d7f666f
Yulia Bobkova <y.bobkova@ps-webforge.com>
2014-08-19 18:19:06 +0200

    add logisAs() to commons

--- tests/js/cucumber/login-step-definitions.js
+++ tests/js/cucumber/login-step-definitions.js
@@ -1,7 +1,29 @@
-var steps = function () {
+var steps = function (expect, commons) {
+
+  commons.loginAs = function(username, password, callback) {
+    var world = this;
+
+    world.browser.authenticate().basic('herrmueller', 'secret123');
+
+    // can have different id if fixture parts are loaded!
+    world.executeDQL(
+      'SELECT user FROM Clieman\\Entities\\User as user WHERE user.username = :name ',
+      { name: username },
+      function(result) {
+        expect(result).to.have.length(1);
+
+        world.userId = result[0].id;
+      },
+      callback
+    );
+  };
 
   this.Given(/^the user is not logged in$/, function(callback) {
     callback();
+  });
+
+  this.Given(/^the user is logged in$/, function(callback) {
+    commons.loginAs.call(this, 'herrmueller', 'secret123', callback);
   });
 };
 

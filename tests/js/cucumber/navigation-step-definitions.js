7c3a8f4d4c6052833b9be891b1b23eec30c8b36c
Yulia Bobkova <y.bobkova@ps-webforge.com>
2014-10-23 19:27:11 +0200

    add wait to some functions

--- tests/js/cucumber/navigation-step-definitions.js
+++ tests/js/cucumber/navigation-step-definitions.js
@@ -6,7 +6,8 @@ module.exports = function(expect, commons) {
     var urls = {
       home: '/',
       clients: '/clients',
-      login: '/login'
+      login: '/login',
+      threeFattiesProjects: '/clients/1/projects'
     };
 
     expect(urls).to.have.property(location);
@@ -18,8 +19,19 @@ module.exports = function(expect, commons) {
     });
   };
 
+  commons.wait = function(msec, callback) {
+    setTimeout(function() {
+      callback();
+    }, msec);
+  };
+
   this.Given(/^the user is at the page "([^"]*)"$/, function(location, callback) {
     commons.goTo.call(this, location);
-    callback();
+    commons.wait.call(this, 6000, callback);
+  });
+
+  this.Given(/^the user is at the page "projects" of the client "Three Fatties"$/, function(callback) {
+    commons.goTo.call(this, 'threeFattiesProjects');
+    commons.wait.call(this, 6000, callback);
   });
 };

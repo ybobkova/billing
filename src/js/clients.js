de0dec9c054d2eb6fcff5201d9e78f7a18e5a439
Yulia Bobkova <y.bobkova@ps-webforge.com>
2014-10-23 19:20:41 +0200

    add functions to operate with clients-observableArray

--- src/js/clients.js
+++ src/js/clients.js
@@ -1,4 +1,4 @@
-define('clients', ['knockout', 'jquery', 'KnockoutSync/Backend', 'KnockoutSync/EntityModel', 'KnockoutSync/jQueryAjaxDriver', 'knockout-mapping'],
+define('clients', ['knockout', 'jquery', 'KnockoutSync/Backend', 'KnockoutSync/EntityModel', 'KnockoutSync/jQueryAjaxDriver', 'knockout-mapping', 'JSON','knockout-projections'],
   function(ko, $, Backend, EntityModel, jQueryAjaxDriver, km) {
 
   var emptyModel = new EntityModel({entities: []});
@@ -7,7 +7,11 @@ define('clients', ['knockout', 'jquery', 'KnockoutSync/Backend', 'KnockoutSync/E
   var that = this;
 
   var clientsViewModel = function () {
-    this.client_name = '';
+    var self = this;
+    self.new_name = ko.observable('');
+    self.search_name = ko.observable('');
+
+    self.edit_name = ko.observable('');
 
     var clientsMapping = {
       'clients': {
@@ -20,16 +24,61 @@ define('clients', ['knockout', 'jquery', 'KnockoutSync/Backend', 'KnockoutSync/E
     km.fromJS({clients:[]}, clientsMapping, this);
     that.updateModel();
 
-    this.addClient = function() {
-      console.log(this.client_name);
-      backend.dispatchRequest('POST', 'clients', {name: this.client_name}, [200], function(failure, result) {});
-      that.updateModel();
-      console.log(clientsViewModel);
+    self.addClient = function() {
+      backend.dispatchRequest('POST', 'clients', {name: self.new_name()}, [204], function(failure) {
+        self.new_name('');
+        if (!failure) {
+          that.updateModel();
+        } else {
+          console.log(failure);
+        }
+      });
     };
 
-    this.removeClient = function() {
-      console.log('remove client');
+    self.removeClient = function(client) {
+      backend.dispatchRequest('DELETE', 'clients/' + client.id(), undefined, [204], function(failure) {
+        if (!failure) {
+          that.updateModel();
+        }
+      });
     };
+
+    self.nullValues = function() {
+      self.edit_name('');
+    };
+
+    self.copyValues = function(client) {
+      self.edit_name(client.name());
+      self.active_id = client.id();
+    };
+
+    self.editClient = function() {
+      backend.dispatchRequest('PUT', 'clients/' + self.active_id, {name: self.edit_name()}, [204], function(failure) {
+        self.nullValues();
+        if (!failure) {
+          that.updateModel();
+        } else {
+          console.log(failure);
+        }
+      });
+    };
+
+    self.filteredClients = ko.dependentObservable(function() {
+      var filter = self.search_name().toLowerCase();
+      if (!filter || filter === '') {
+        return self.clients();
+      } else {
+        return ko.utils.arrayFilter(self.clients(), function(client) {
+          return (client.name().toLowerCase().indexOf(filter) > -1);
+        });
+      }
+    }, self.clients);
+
+    self.openProjects = function(client) {
+      var id = client.id();
+      window.document.location = 'clients/' + id + '/projects';
+    };
+
   };
 
   this.updateModel = function () {
@@ -40,11 +89,4 @@ define('clients', ['knockout', 'jquery', 'KnockoutSync/Backend', 'KnockoutSync/E
 
   var clients = new clientsViewModel();
   ko.applyBindings(clients);
-
-  jQuery(document).ready(function($) {
-    $(".clickableRow")
-      .click(function() {
-        window.document.location = $(this).attr("href");
-      });
-  });
 });
